d2f4e9178f06a1c63f3e9338c68f9b85930dfd6d
Meshach Nsude <nsudeme777@gmail.com>
2025-01-18 11:03:05 +0100

    save letter spacing changes

--- src/components/global/BracesHeaderText.tsx
+++ src/components/global/BracesHeaderText.tsx
@@ -9,7 +9,7 @@ const BracesHeaderText:React.FC<Props> = ({text}) => {
     <div className='group relative opacity-50 hover:opacity-100 transition-all duration-[600ms]'>
       <span className='hidden lg:block text-[40px] opacity-0 absolute left-[-15px] top-[-8px] group-hover:left-0 group-hover:opacity-100 transition-all duration-[200ms]'>{'{'}</span>
       <h2 
-        className="text-[40px] leading-[1.1] tracking-tight lg:mx-5">
+        className="text-[40px] leading-[1.1] tracking-[-0.02ch] lg:mx-5">
         {text}
       </h2>
       <span className='hidden lg:block text-[40px] opacity-0 absolute right-[-15px] top-[-8px] group-hover:right-0 group-hover:opacity-100 transition-all duration-[200ms]'>{'}'}</span>
