735e3cd44a994a8061440329a149fbb0688aedf2
Meshach Nsude <nsudeme777@gmail.com>
2025-02-10 11:40:52 +0100

    save changes

--- src/components/junk-lab/JunklabDesktop.tsx
+++ src/components/junk-lab/JunklabDesktop.tsx
@@ -11,7 +11,7 @@ const JunklabDesktop = () => {
 
   return (
     <div className="text-white">
-      <div className="h-screen w-full grid grid-cols-8 items-center">
+      <div className="h-screen w-full mb-10 grid grid-cols-8 items-center">
         <h2 className="text-[40px] tracking-[0.01ch] leading-[1] col-start-2 col-span-6">
           &nbsp;&nbsp;&nbsp;&nbsp; Welcome to the Junk lab, a curated collection of layouts from my 365 days of typography journey.
           Before you dive in, don’t forget your shades—this mind-altering journey is not for the faint of heart. May the design forces be with you.
--- src/components/junk-lab/ListPosterLayout.tsx
+++ src/components/junk-lab/ListPosterLayout.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import useCustomEffect from "../hooks/useCustomEffect";
 import { PosterLayout } from "../models";
 import gsap from "gsap";
@@ -10,46 +10,65 @@ gsap.registerPlugin(ScrollTrigger);
 const ListPosterLayout = ({posters}: PosterLayout) => {
   const containerRef = useRef(null);
   const nameListRef = useRef(null); // container for overflow
-  const namesRef = useRef(null); // div containing the names directly
   const nameDisplayCon = useRef(null);
+  const selectorRef = useRef(null);
+  const pointerRef = useRef(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   useCustomEffect(() => {
     if (!containerRef.current || !nameListRef.current) return;
+    const names = nameListRef.current as HTMLDivElement;
+    const container = containerRef.current as HTMLDivElement;
 
     gsap.killTweensOf(containerRef.current);
 
-    const animation = ScrollTrigger.create({
-      trigger: containerRef.current,
-      markers: {
-        startColor: "green",
-        endColor: "red"
-      }, 
-      start: "top-=400 top",
-      end: "bottom top",
-      pin: true,
-      onEnter: () => {
-        showDisplayCon();
-      },
-      onEnterBack: () => {
-        showDisplayCon();
-      },
-      onLeave: () => {
-        hideDisplayCon();
-      },
-      onLeaveBack: () => {
-        hideDisplayCon();
-      }
-    })
+    const maxScroll = names.scrollHeight - names.offsetHeight + window.innerHeight / 4;
 
-    return () => animation.kill();
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container,
+        start: "top top",
+        end: "max",
+        scrub: true,
+        // markers: { startColor: "green", endColor: "red" },
+        pin: true,
+        onEnter: showDisplayCon,
+        onEnterBack: showDisplayCon,
+        onLeave: hideDisplayCon,
+        onLeaveBack: hideDisplayCon
+      }
+    });
+
+    // scroll animation for name list
+    tl.fromTo(names, { y: 0 }, { y: -maxScroll, ease: "none" });
+
+    return () => {
+      tl.scrollTrigger?.kill();
+      tl.kill();
+    };
   })
 
   const showDisplayCon = () => {
-    gsap.to(containerRef.current, {opacity: 1, duration: .4});
+    gsap.to(containerRef.current, {opacity: 1, pointerEvents: "all", duration: 1});
   }
 
   const hideDisplayCon = () => {
-    gsap.to(containerRef.current, {opacity: 0, duration: .4});
+    gsap.to(containerRef.current, {opacity: 0, pointerEvents: "none", duration: 1});
+  }
+
+  const handleItemClick = (e: React.MouseEvent, i: number) => {
+    if (!pointerRef.current) return;
+    setSelectedIndex(i);
+    const pointer = pointerRef.current as HTMLDivElement;
+    const target = e.currentTarget as HTMLButtonElement;
+    const duration = 0.6;
+    const ease = "expo.inOut";
+    
+    gsap.to(pointer, {
+      y: target.offsetTop,
+      ease,
+      duration
+    })
   }
 
   return (
@@ -60,18 +79,26 @@ const ListPosterLayout = ({posters}: PosterLayout) => {
       {/* Post preview  */}
       <div></div>
 
+      {/* ===== SELECTOR ===== */}
+      <div ref={selectorRef} className="fixed border-2 border-red-500 w-full h-[30px] left-0 top-[50%] -translate-y-[50%]" />
       {/* Name List and Display */}
       <div ref={nameDisplayCon} className="col-start-3 col-span-5">
-        <div ref={nameListRef} className=" text-[25px] leading-[1] h-[75vh] overflow-hidden">
-          <div ref={namesRef} className="flex flex-col gap-y-[10px]">
-            {
-              posters.map((poster) => (
-                <button key={poster.name} className="text-left w-fit opacity-40 transition-opacity duration-[400ms] hover:opacity-100">
-                  {poster.name}
-                </button>
-              ))
-            }
-          </div>
+        <div ref={nameListRef} className="relative text-[25px] flex flex-col gap-y-2.5 leading-[1] h-[75vh] mt-[50vh] w-fit overflow-visible">
+
+         {/* Triangle Pointer */}
+         <div 
+          ref={pointerRef}
+          className="absolute -right-[60px] w-0 h-0 border-l-[8px] border-r-[8px] border-b-[16px] border-l-transparent border-r-transparent border-b-myGray-100 -rotate-90"/>
+          {
+            posters.map((poster, i) => (
+              <button 
+                key={poster.name} 
+                onClick={(e) => handleItemClick(e, i)}
+                className={`text-left w-fit transition-opacity duration-[600ms] hover:opacity-100 ${selectedIndex === i ? 'opacity-100' : 'opacity-40'}`}>
+                {poster.name}
+              </button>
+            ))
+          }
         </div>
         <div></div>
       </div>
