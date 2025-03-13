7bc3bdaf4987f4cb37756ae8002e4f8905b94e3b
Meshach Nsude <nsudeme777@gmail.com>
2025-03-13 10:40:13 +0100

    try 5

--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -14,6 +14,7 @@ const ProjectGrid = ({project}: Props) => {
       return navigate("/projects/ui-layouts");
     }
 
+    console.log(project.title)
     navigate(`/projects/${project.title}`);
   }
 
